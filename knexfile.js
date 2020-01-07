712db7c70f0af56a1d994167fd20a75ad2ed41b6
theolamide <ajadidan@gmail.com>
2020-01-07 11:34:51 -0500

    ReadMe updates

--- knexfile.js
+++ knexfile.js
@@ -42,7 +42,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'process.env.DATABASE_URL',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './database/migrations',
     },
